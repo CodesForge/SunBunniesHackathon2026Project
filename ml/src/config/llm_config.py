85a12f81f50949f7bf397c37bd5edf6d821e2940
skrett <golakovsergej1@gmail.com>
2026-09-20 13:09:37 +0300

    feat: add LLM configuration

---
+++ ml/src/config/llm_config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from pydantic import SecretStr, Field
+from functools import lru_cache
+
+class LLM_settings(BaseSettings):
+    auth_key: SecretStr = Field(min_length=1)
+    scope: str = Field(min_length=1)
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=True
+    )
+
+@lru_cache(maxsize=1)
+def get_llm_settings() -> LLM_settings:
+    return LLM_settings
+
+
