7eeb3bf5f682ff95777282558a8510a464ff3402
skrett <golakovsergej1@gmail.com>
2026-09-20 22:35:51 +0300

    feat(ml/client): add response format

--- ml/src/client/llm_client.py
+++ ml/src/client/llm_client.py
@@ -1,14 +1,16 @@
 from gigachat import Chat, GigaChatAsyncClient
-from gigachat.exceptions import AuthenticationError, ModelNotSpecifiedError, NotFoundError
+from gigachat.exceptions import GigaChatException
+from gigachat.models import JsonSchemaResponseFormat
 
 from src.config.llm_config import get_llm_settings as s
 from src.models.llm_completion import LLMGenerationConfig
 
 
 async def generate_llm_completion(
-    config: LLMGenerationConfig
-) :
-    try :
+    config: LLMGenerationConfig,
+    response_format: JsonSchemaResponseFormat | None = None
+) -> str:
+    try:
         async with GigaChatAsyncClient(
             credentials=s.auth_key,
             scope=s.scope,
@@ -23,28 +25,12 @@ async def generate_llm_completion(
             payload = Chat(
                 messages=config.messages,
                 reasoning_effort=config.reasoning_effort,
-                temperature=config.temperature
+                temperature=config.temperature,
+                response_format=response_format
             )
 
             response = await client.achat(payload=payload)
-            return response.choices[0].message.content
-        
-    except AuthenticationError as exc:
-        return {
-            "error": "Ошибка авторизации",
-            "status_code": {exc.status_code},
-            "content": {exc.content}
-        }
-    except ModelNotSpecifiedError as exc:
-        return {
-            "error": "Модель не указана в параметрах",
-            "status_code": {exc.status_code},
-            "content": {exc.content}
-        }
-    except NotFoundError as exc:
-        return {
-            "error": "Модель не найдена",
-            "status_code": {exc.status_code},
-            "content": {exc.content}
-        }
-    
+    except GigaChatException as exc:
+        return f"Failed to get a response: {exc}"
+
+    return response.choices[0].message.content
