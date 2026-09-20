3bf9fb6bec2589a10b67ad25c7d4c276dc1b2782
Sirax67 <abrakate20@gmail.com>
2026-09-20 15:43:20 +0300

    chore(mobile): add empty start screen

---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function StartScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Монетка</Text>
+    </View>
+  );
+}
