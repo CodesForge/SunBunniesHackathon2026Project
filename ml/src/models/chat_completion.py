73be9cab38e10a1bf83487902a451b223824a95c
skrett <golakovsergej1@gmail.com>
2026-09-20 19:10:21 +0300

    feat(models): add configuration for the request to the LLM.

---
+++ ml/src/models/chat_completion.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+from gigachat import Messages
+from pydantic import BaseModel, Field
+
+
+class LLMGenerationConfig(BaseModel):
+    messages: list[Messages] 
+    reasoning_effort: Literal['low', 'medium', 'high'] | None = Field(
+        default=None,
+        description="Model reasoning depth level"
+    )
+    temperature: float | None = Field(
+        default=None,
+        ge=0.0,
+        le=2.0,
+        description="The higher, the more random the answer."
+    )
